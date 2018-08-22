9a210eb1e64968a07fc9291cf7a20d76b0b74870
sanwenjing <sanwenjing@163.com>
2018-08-22 17:00:15 +0800

    增加操作系统判断

--- com.py
+++ com.py
@@ -2,14 +2,14 @@
 # -*- coding: UTF-8 -*-
 import os,sys;
 import datetime as dt;
-
+import platform as pf;
 class log:
       fd="";
       def __init__(self,fileaddr=""):
             if(fileaddr):
                   self.fd=fileaddr;
             else:
-                  self.fd=os.getcwd()+"\\"+ getFileName()+".log";
+                  self.fd=getAddrStr(os.getcwd()+"\\"+ getFileName()+".log");
       def getFd(self):
             return(self.fd);
       def w(self,text):
@@ -17,6 +17,11 @@ class log:
             of.write(getTime()+"  "+text+"\r\n");
             of.close();
             return;
+def getAddrStr(address):
+      if getSystemClass()=="Windows":
+            return address.replace("\/","\\");
+      else:
+            return address.replace("\\","\/");
 def getTime():
       return getTimeFormat('%Y-%m-%d %H:%M:%S');
 def getTimeFormat(format):
@@ -26,7 +31,9 @@ def getTimeFormat(format):
 def getFileName():
     fn=sys.argv[0][sys.argv[0].rfind(os.sep) + 1:];
     return fn.split(".")[0];
-	
+
+def getSystemClass():
+      return pf.system();
 if __name__=="__main__":
          print(getFileName());
          log1=log();
