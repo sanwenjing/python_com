fee16394071c9b6e25bc9f087fc1e3513ad9d0eb
sanwenjing <sanwenjing@163.com>
2018-08-22 16:27:36 +0800

    初始化提交

---
+++ com.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+import os,sys;
+import datetime as dt;
+
+class log:
+      fd="";
+      def __init__(self,fileaddr=""):
+            if(fileaddr):
+                  self.fd=fileaddr;
+            else:
+                  self.fd=os.getcwd()+"\\"+ getFileName()+".log";
+      def getFd(self):
+            return(self.fd);
+      def w(self,text):
+            of=open(self.fd,'a');
+            of.write(getTime()+"  "+text+"\r\n");
+            of.close();
+            return;
+def getTime():
+      now=dt.datetime.now();
+      return now.strftime('%Y-%m-%d %H:%M:%S');
+
+	
+def getFileName():
+    fn=sys.argv[0][sys.argv[0].rfind(os.sep) + 1:];
+    return fn.split(".")[0];
+	
+if __name__=="__main__":
+         print(getFileName());
+         log1=log();
+         print(log1.getFd());
+         log1.w("test");
