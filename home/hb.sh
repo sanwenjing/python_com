0d8d86967839f736083595a60b6f9842e149f448
sanwenjing <sanwenjing@163.com>
2018-11-10 17:24:21 +0800

    update hb.sh

--- home/hb.sh
+++ home/hb.sh
@@ -1,12 +1,36 @@
 #!/bin/sh
 
 #check the homebridge run status
-status=`/etc/init.d/homebridge status`
-echo $status
+bin="homebridge"
+start(){
+status=`/etc/init.d/$bin status`
 if [ "$status" = "Stopped" ] 
 then
-	`/etc/init.d/homebridge start>/dev/null 2>&1`
-	echo "Starting homebridge now"
+	`/etc/init.d/$1 start>/dev/null 2>&1`
+	echo "Starting $1 now"
 else 
-	echo "The homebridge is "$status
+	echo "The $1 is "$status
 fi
+}
+stop(){
+	`ps -ef|grep $1|grep -v grep|awk '{print $2}'|xargs kill -9`
+	echo "$1 stoped!"
+}
+case $1 in
+"start")
+	start $bin
+;;
+"stop")
+	stop $bin
+;;
+"restart")
+	stop $bin
+	start $bin
+;;
+"status")
+	echo "$bin status:`/etc/init.d/$bin status`"
+;;
+*)
+	echo "usage: start|stop|restart|status"
+;;
+esac
